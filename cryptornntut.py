f3c78eebcdda2af7a633403ac143fad8fde68b0b
iribaboci <tahirbaboci96@gmail.com>
2018-12-10 16:01:22 +0300

    added changes on preprocess_df function

--- cryptornntut.py
+++ cryptornntut.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import os
 from sklearn import preprocessing
 from collections import deque
 import numpy as np
@@ -27,8 +26,9 @@ def classify(current, future):
 
 
 def preprocess_df(df):
+    df = test_main_df
     df = df.drop('future', 1)
-
+    
     for col in df.columns:
         if col != "target":
             df[col] = df[col].pct_change() # ?
@@ -38,7 +38,7 @@ def preprocess_df(df):
 
     df.dropna(inplace=True) # drop if it creates a "not a number":na
     sequencial_data = []
-    prev_days = deque(maxLen=SEQ_LEN) #prev_days is a deque with a max length of 60...
+    prev_days = deque(maxlen=SEQ_LEN) #prev_days is a deque with a max length of 60...
     #Deques is like a list, you just keep appending to this list, but as list reaches the length of 60  it will pop out the old items and add new ones
 
     for i in df.values:  #convers dataframe to list of lists
@@ -48,24 +48,56 @@ def preprocess_df(df):
             sequencial_data.append([np.array(prev_days), i[:-1]]) # we are going to append our features and labels , x and y
 
     random.shuffle(sequencial_data) # ?? why
+    #print(sequencial_data)
+    buys = {}
+    sells = {}
+
+    for seq, target in sequencial_data:
+        if target == 0: # this is a sell, means it goes down
+            sells.append([seq, target])
+        elif target == 1:
+            buys.append([seq, target])
+    #shuffle for good mesure :D
+    random.shuffle(buys)
+    random.shuffle(sells)
+
+    # we need to find which one is lesser(we are going to find which one is the minimum)
+    lower = min(len(buys), len(sells))
+    # buys up too lower
+    buys = buys[:lower] # ?
+    #sells up too lower
+    sells = sells[:lower] # ?
+
+    sequencial_data = buys + sells
+    random.shuffle(sequencial_data)
+
+    X = []
+    y = []
+
+    for seq, target in sequencial_data:
+        X.append(seq)
+        y.append(target)
+
+    return np.array(X), y
 
 
-
-
-
+#pd.show_versions()
 
 ratios = ['BTC-USD', 'LTC-USD', 'ETH-USD', 'BCH-USD'] # files that are going to be used
 for ratio in ratios:
-    dataset = f"crypto_data/{ratio}.cvs"
+    dataset = f"crypto_data/{ratio}.csv"
 
     df = pd.read_csv(dataset, names=['time','low','high','open','close','volume'])
     #print(df.head())
-    df.rename(columns={"close" : f"{ratio}_close", "volume" : f"{ratio}_volume"}, inframe=True) #inframe is in case so we dont need to redifine dataframe
+    df.rename(columns={"close" : f"{ratio}_close", "volume" : f"{ratio}_volume"}, inplace=True) #inplace is in case so we dont need to redifine dataframe
 
+    
     df.set_index("time", inplace=True)
-    df = df[[f"{ratio}_close", f"{ratio}_volume"]]
-
-    #print(df.head())
+    #df = df.drop('low', 1) #0 means index 1 means column
+    #df = df.drop('high', 1)
+    df = df[[f"{ratio}_close", f"{ratio}_volume"]]  # ignore the other columns besides price and volume
+    
+    print(df.head())
 
     if len(main_df) == 0:
         main_df = df
@@ -113,6 +145,10 @@ test_main_df = main_df[(main_df.index >= last_5percent)] # it takes last 5% of t
 main_df = main_df[(main_df.index < last_5percent)]  # it takes the rest (first 95%)
 
 
-preprocess_df(main_df)
-#train_x, train_y = preprocess_df(main_df)
-#test_x, test_y = preprocess_df(test_main_df)
+train_x, train_y = preprocess_df(main_df)
+test_x, test_y = preprocess_df(test_main_df)
+
+
+print(f"train data: {len(train_x)} validation: {len(test_x)}")
+print(f"Dont buys: {train_y.count(0)}. buys: {train_y.count(1)}")
+print(f"TEST Dont buys: {test_y.count(0)}, buys: {test_y.count(1)}")
